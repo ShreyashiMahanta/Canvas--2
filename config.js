54aed7088c84bda689ad79a245295e505396805f
ShreyashiMahanta <53649054+ShreyashiMahanta@users.noreply.github.com>
2020-04-18 12:38:38 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+  apiKey: "AIzaSyD0iOZzqTe22qSj8HnmT8_6YGXys_R2B-Y",
+  authDomain: "world-canvas-bdeac.firebaseapp.com",
+  databaseURL: "https://world-canvas-bdeac.firebaseio.com",
+  projectId: "world-canvas-bdeac",
+  storageBucket: "world-canvas-bdeac.appspot.com",
+  messagingSenderId: "241697226737",
+  appId: "1:241697226737:web:67ea46d661e1d065b28e50"
+};
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
